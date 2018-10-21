045c334307ec8e3e78f9312f4693b270fd0ce4ab
jack <jackadam@sina.com>
2018-10-21 15:24:08 +0800

    语法结构

--- 01-语法结构/12.装饰器.py
+++ 01-语法结构/12.装饰器.py
@@ -3,7 +3,7 @@
 # @Time    : 2018-07-29 22:28
 # @Author  : Jackadam
 # @Email   :jackadam@sina.com
-# @File    : 12.装饰器.py
+# @File    : 02.Example.py
 # @Software: PyCharm
 #装饰器就是函数调用函数。
 
@@ -16,3 +16,4 @@ def func2(a,b):
     func1(a,b)
 
 func2(2,3)
+
